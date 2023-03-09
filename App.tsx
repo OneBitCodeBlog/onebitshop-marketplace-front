f59475f08901ac84b6e0164e5c53895865c3aeeb
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-03-09 02:21:52 -0300

    Configuração da navegação e implementação dela no App.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from "./src/routes";
+import { StatusBar } from "expo-status-bar";
+
+import { ThemeProvider } from "styled-components/native";
+import { myTheme } from "./src/styles";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <ThemeProvider theme={myTheme}>
+        <Routes />
+      </ThemeProvider>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+
+import Home from "../screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+type PropsNavigationStack = {
+  Home: undefined;
+};
+
+export type PropsStack = NativeStackNavigationProp<PropsNavigationStack>;
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
