9d1863fc8863a7ab49a45161cfb6ce3279ae9888
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-03-17 13:56:43 -0300

    Implementação do navbar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../components/common/Header";
+import NavBar from "../../components/common/NavBar";
 import { Container } from "./styled";
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <NavBar />
     </Container>
   );
 };
