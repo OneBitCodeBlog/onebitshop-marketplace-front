2b202dc0f93e4162acd1b8ce68baa8e2d7807bb3
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-03-17 13:34:01 -0300

    Atualiação no import das imagens do header

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Container, Input, InputContainer, Logo, Search } from "./styled";
 
-const logo = require("../../../../../assets/images/horizontal-logo.png");
-const search = require("../../../../../assets/icons/search.png");
+const logo = require("../../../../assets/images/horizontal-logo.png");
+const search = require("../../../../assets/icons/search.png");
 
 const Header = () => {
   return (
