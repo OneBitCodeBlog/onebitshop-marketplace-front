a2f3e069e72fd795d77029c5bd8e48194cb81c1b
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-03-17 14:59:35 -0300

    Implementação do productList

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../components/common/Header";
 import NavBar from "../../components/common/NavBar";
+import ProductList from "../../components/common/ProductList";
 import { Container } from "./styled";
 
 const Home = () => {
   return (
     <Container>
       <Header />
+      <ProductList />
       <NavBar />
     </Container>
   );
