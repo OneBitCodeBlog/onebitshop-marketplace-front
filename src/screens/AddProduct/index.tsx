d96a47a02efb4238d38cd1bd89a0ade8d2145dd3
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-05-09 17:44:30 -0300

    Criando os campos comuns de AddProduct

--- src/screens/AddProduct/index.tsx
+++ src/screens/AddProduct/index.tsx
@@ -1,12 +1,85 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Container } from "./styled";
+import React, { useState } from "react";
+import {
+  Container,
+  DescriptionContainer,
+  Division,
+  Input,
+  InputContainer,
+} from "./styled";
 import DefaultTitle from "../../components/common/DefaultTitle";
+import DropDownComponent from "../../components/common/DropDownComponent";
+import DefaultButton from "../../components/common/DefaultButton";
+
+const Address = [
+  { value: "Endereço 1" },
+  { value: "Endereço 2" },
+  { value: "Endereço 3" },
+  { value: "Endereço 4" },
+];
+
+const Categorie = [
+  { value: "Categoria 1" },
+  { value: "Categoria 2" },
+  { value: "Categoria 3" },
+  { value: "Categoria 4" },
+  { value: "Categoria 4" },
+  { value: "Categoria 4" },
+  { value: "Categoria 4" },
+  { value: "Categoria 4" },
+];
 
 const AddProduct = () => {
+  const [categorie, setCategorie] = useState("");
+  const [address, setAddress] = useState("");
+
   return (
-    <Container>
+    <Container contentContainerStyle={{ paddingBottom: 120 }}>
       <DefaultTitle title="CADASTRO DO ANÚNCIO" fontSize={20} />
+
+      <InputContainer>
+        <Input placeholder="Título" />
+      </InputContainer>
+
+      <InputContainer>
+        <Input placeholder="Preço" keyboardType="numeric" />
+      </InputContainer>
+
+      <DescriptionContainer>
+        <Input placeholder="Descrição" />
+      </DescriptionContainer>
+
+      <InputContainer>
+        <Input placeholder="IMAGEM (SIMBÓLICO)" />
+      </InputContainer>
+
+      <DropDownComponent
+        data={Categorie}
+        placeholder="Selecione a categoria"
+        setSelected={setCategorie}
+      />
+
+      <DropDownComponent
+        data={Address}
+        placeholder="Selecione um endereço"
+        setSelected={setAddress}
+      />
+
+      <DefaultButton
+        buttonText="CADASTRAR E PUBLICAR"
+        buttonType="primary"
+        buttonHandle={() => {}}
+        marginVertical={20}
+      />
+
+      <Division>Ou</Division>
+
+      <DefaultButton
+        buttonText="SALVAR COMO RASCUNHO"
+        buttonType="secondary"
+        buttonHandle={() => {}}
+        marginVertical={20}
+      />
     </Container>
   );
 };
--- src/screens/AddProduct/styled.ts
+++ src/screens/AddProduct/styled.ts
@@ -8,3 +8,31 @@ export const Container = styled.ScrollView`
   background-color: ${({ theme }) => theme.colors.backgroundLight};
   padding-top: ${statusBarHeight}px;
 `;
+
+export const InputContainer = styled.View`
+  width: 90%;
+  height: 50px;
+  padding-left: 10px;
+  border: 1px solid ${({ theme }) => theme.colors.borderColor};
+  background-color: ${({ theme }) => theme.colors.background};
+  border-radius: 5px;
+  margin: 10px auto;
+`;
+
+export const DescriptionContainer = styled(InputContainer)`
+  min-height: 130px;
+`;
+
+export const Input = styled.TextInput.attrs({
+  placeholderTextColor: "white",
+})`
+  flex: 1;
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.primaryText};
+`;
+
+export const Division = styled.Text`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.secondaryText};
+  text-align: center;
+`;
