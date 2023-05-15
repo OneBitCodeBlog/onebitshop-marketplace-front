ca979fb0c5699145c87f82e0385688cb0f9c710a
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-05-15 11:56:20 -0300

    Criando a lista de chats

---
+++ src/components/AllChats/ChatList/styled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+
+export const NoChat = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  text-align: center;
+  color: ${({ theme }) => theme.colors.primaryText};
+  margin-top: 40px;
+`;
--- src/screens/AllChats/index.tsx
+++ src/screens/AllChats/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Container } from "./styled";
 import DefaultTitle from "../../components/common/DefaultTitle";
 import NavBar from "../../components/common/NavBar";
+import ChatList from "../../components/AllChats/ChatList";
 
 const AllChats = () => {
   return (
     <>
       <Container>
         <DefaultTitle title="CONVERSAS" fontSize={20} />
+
+        <ChatList />
       </Container>
       <NavBar />
     </>
