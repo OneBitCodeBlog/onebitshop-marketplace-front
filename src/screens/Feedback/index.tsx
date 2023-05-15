bb0aee67a3830655f0fe887d608ea820300ee2d5
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-05-15 11:29:37 -0300

    Criando a lógica de avaliação

--- src/screens/Feedback/index.tsx
+++ src/screens/Feedback/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Container } from "./styled";
+import { Container, FeedbackText } from "./styled";
 import DefaultTitle from "../../components/common/DefaultTitle";
 import ProfileInfo from "../../components/common/ProfileInfo";
 import NavBar from "../../components/common/NavBar";
+import { AirbnbRating } from "react-native-ratings";
+import DefaultButton from "../../components/common/DefaultButton";
 
 const Feedback = () => {
   return (
@@ -10,6 +12,24 @@ const Feedback = () => {
       <DefaultTitle title="AVALIAR" fontSize={20} />
 
       <ProfileInfo />
+
+      <FeedbackText>
+        Dê uma nota de 1 a 5, sendo{"\n"}1 estrela sendo ruim e 5 excelente
+      </FeedbackText>
+
+      <AirbnbRating
+        selectedColor="#5F96ED"
+        showRating={false}
+        size={40}
+        defaultRating={0}
+      />
+
+      <DefaultButton
+        buttonText={"ENVIAR AVALIAÇÃO"}
+        buttonType={"primary"}
+        marginVertical={80}
+        buttonHandle={() => {}}
+      />
 
       <NavBar />
     </Container>
--- src/screens/Feedback/styled.ts
+++ src/screens/Feedback/styled.ts
@@ -8,3 +8,10 @@ export const Container = styled.View`
   background-color: ${({ theme }) => theme.colors.backgroundLight};
   padding-top: ${statusBarHeight}px;
 `;
+
+export const FeedbackText = styled.Text`
+  font-size: 18px;
+  color: ${({ theme }) => theme.colors.primaryText};
+  text-align: center;
+  margin: 40px 0px;
+`;
