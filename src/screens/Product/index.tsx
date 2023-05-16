b59a2b6a9d1387d6156ee110d0344234b80629fb
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-05-16 14:46:21 -0300

    Estruturando o título

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { Container } from "./styled";
+import { Container, SubTitle, SubtitleContainer } from "./styled";
 import BackIcon from "../../components/common/BackIcon";
+import { Title } from "./styled";
 
 const Product = () => {
   return (
     <Container>
       <BackIcon marginLeft={30} />
+
+      <Title>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa,
+        tenetur?
+      </Title>
+      <SubtitleContainer>
+        <SubTitle>Publicado em 10/04/23</SubTitle>
+        <SubTitle>Recife, PE</SubTitle>
+      </SubtitleContainer>
     </Container>
   );
 };
--- src/screens/Product/styled.ts
+++ src/screens/Product/styled.ts
@@ -8,3 +8,21 @@ export const Container = styled.ScrollView`
   background-color: ${({ theme }) => theme.colors.backgroundLight};
   padding-top: ${statusBarHeight}px;
 `;
+
+export const Title = styled.Text`
+  font-size: 22px;
+  color: ${({ theme }) => theme.colors.primaryText};
+  margin: 0px 30px;
+  font-weight: bold;
+`;
+
+export const SubtitleContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0px 30px;
+`;
+
+export const SubTitle = styled.Text`
+  font-size: 14px;
+  color: ${({ theme }) => theme.colors.secondaryText};
+`;
