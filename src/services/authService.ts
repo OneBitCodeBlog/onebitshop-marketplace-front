d23c1493000f377ca11316b89b1d830660111c0d
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-06-05 15:26:08 -0300

    Criação do serviço de login

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,4 +1,7 @@
 import api from "./api";
+import { Alert } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface RegisterParams {
   name: string;
@@ -7,9 +10,26 @@ interface RegisterParams {
   phone: string;
 }
 
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
 const authService = {
   register: async (params: RegisterParams) => {
     const res = await api.post("/register", params);
+
+    return res;
+  },
+  login: async (params: LoginParams) => {
+    const res = await api.post("/login", params);
+
+    if (res.status === 400 || res.status === 401) {
+      Alert.alert("Email ou senha incorretos");
+    } else {
+      await SecureStore.setItemAsync("onebitshop-token", res.data.token);
+      await AsyncStorage.setItem("user", JSON.stringify(res.data.user));
+    }
 
     return res;
   },
