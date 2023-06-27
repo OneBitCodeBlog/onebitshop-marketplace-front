000a2961dfc8e5fbdc4794565d83ba65b768a4a0
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-06-27 10:40:34 -0300

    Uitilizando o serviço de search

--- src/screens/Search/index.tsx
+++ src/screens/Search/index.tsx
@@ -1,21 +1,51 @@
 import { View, Text } from "react-native";
-import React from "react";
-import { Container } from "./styled";
+import React, { useEffect, useState } from "react";
+import { Container, NoResult } from "./styled";
 import Header from "../../components/common/Header";
 import NavBar from "../../components/common/NavBar";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { PropsNavigationStack } from "../../routes";
 import ProductList from "../../components/common/ProductList";
+import { Product } from "../../entities/Product";
+import searchService from "../../services/searchService";
+import Loader from "../Loader";
 
 type Props = NativeStackScreenProps<PropsNavigationStack, "Search">;
 
 const Search = ({ route }: Props) => {
+  const [data, setData] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
   const query = route?.params.query;
+
+  const handleSearch = async () => {
+    const { data } = await searchService.getSearched(query);
+
+    setData(data.products);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [query]);
 
   return (
     <Container>
       <Header />
-      <ProductList />
+
+      {!loading ? (
+        data.length <= 0 ? (
+          <NoResult>
+            Sua pesquisa não trouxe nenhum{"\n"}produto, tente novamente mais
+            tarde!
+          </NoResult>
+        ) : (
+          <ProductList handleGetProducts={handleSearch} products={data} />
+        )
+      ) : (
+        <Loader />
+      )}
+
       <NavBar />
     </Container>
   );
--- src/screens/Search/styled.ts
+++ src/screens/Search/styled.ts
@@ -4,3 +4,11 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.backgroundLight};
 `;
+
+export const NoResult = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 60%;
+  color: white;
+`;
